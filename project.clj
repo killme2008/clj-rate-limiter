195993c3fe65afad90ee206d34a1e15cdec3f7ff
dennis zhuang <killme2008@gmail.com>
2015-10-09 15:10:47 +0800

    Released 0.1.2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-rate-limiter "0.1.1"
+(defproject clj-rate-limiter "0.1.2"
   :description "Rate limiter for clojure that supports a rolling window, either in-memory or backed by redis"
-  :url ""
+  :url "https://github.com/killme2008/clj-rate-limiter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
